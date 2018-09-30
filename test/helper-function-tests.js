9cce5a7f731d1e8ebba46671c65cb1f94dfb2ded
Justin Reich <justinreich.dev@gmail.com>
2018-09-29 20:38:10 -0500

    add mocha chai testing to project

---
+++ test/helper-function-tests.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+
+describe('Test Suite', () => {
+    it ('should be a valid test', () => {
+        assert.equal(1, 1);
+    })
+})
